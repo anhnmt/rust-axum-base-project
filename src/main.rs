2c11dc400e15b12220daf6d9f2da77461fc2f90d
Nguyen Manh Tuan Anh <anhng.dev@gmail.com>
2022-04-29 14:05:44 +0700

    Update middleware request, response

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ extern crate dotenvy;
 
 use std::net::SocketAddr;
 
-use axum::{response::Html, Router, routing::get};
-use axum::handler::Handler;
-use axum::http::{Method, StatusCode};
-use axum::response::IntoResponse;
+use axum::{
+    body::{Body, Bytes},
+    handler::Handler,
+    http::{Method, Request, StatusCode},
+    middleware::{self, Next},
+    response::{Html, IntoResponse, Response},
+    Router,
+    routing::{get, post},
+};
 use dotenvy::dotenv;
 use log::info;
 use tokio::signal;
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::{
+    cors::{Any, CorsLayer},
+};
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
@@ -30,6 +37,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
+        .route("/", post(|| async move { "Hello from `POST /`" }))
+        .layer(middleware::from_fn(print_request_response))
         .layer(
             // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
             // for more details
@@ -78,4 +87,40 @@ async fn shutdown_signal() {
     }
 
     info!("Signal received, starting graceful shutdown");
+}
+
+async fn print_request_response(req: Request<Body>, next: Next<Body>) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let (parts, body) = req.into_parts();
+    let bytes = buffer_and_print("request", body).await?;
+    let req = Request::from_parts(parts, Body::from(bytes));
+
+    let res = next.run(req).await;
+
+    let (parts, body) = res.into_parts();
+    let bytes = buffer_and_print("response", body).await?;
+    let res = Response::from_parts(parts, Body::from(bytes));
+
+    Ok(res)
+}
+
+async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, (StatusCode, String)>
+    where
+        B: axum::body::HttpBody<Data=Bytes>,
+        B::Error: std::fmt::Display,
+{
+    let bytes = match hyper::body::to_bytes(body).await {
+        Ok(bytes) => bytes,
+        Err(err) => {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                format!("failed to read {} body: {}", direction, err),
+            ));
+        }
+    };
+
+    if let Ok(body) = std::str::from_utf8(&bytes) {
+        info!("{} body = {:?}", direction, body);
+    }
+
+    Ok(bytes)
 }
